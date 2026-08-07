c2128b545993f67a6a536092db3a7cda29de8bc6
Adam Siudzinski <adam.siudzinski@gmail.com>
2026-08-07 11:55:43 +0200

    Add gallery JavaScript

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,27 @@
 function activateGallery() {
-	alert("Hello from a gallery file");
+	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+	let mainImage = document.querySelector("#gallery-photo > img");
+	let currentClass = "current";
+	let title = document.querySelector("#gallery-info .title");
+	let description = document.querySelector("#gallery-info .description");
+
+	thumbnails.forEach(function(thumbnail) {
+		let newImageSrc = thumbnail.dataset.largeVersion
+		let largeVersion = new Image();
+		largeVersion.src = newImageSrc;
+
+		thumbnail.addEventListener("click", function() {
+			// set clicked image as display image
+			mainImage.setAttribute("src", newImageSrc);
+			mainImage.setAttribute("alt", thumbnail.dataset.title);
+
+			// change which image is current
+			document.querySelector("." + currentClass).classList.remove(currentClass);
+			thumbnail.parentNode.classList.add(currentClass);
+
+			// change display image title and description in third column
+			title.innerHTML = thumbnail.dataset.title;
+			description.innerHTML = thumbnail.dataset.description;
+		});
+	});
 }
